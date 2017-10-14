99634d9053d34a43f48dd355826fe9d5d9a4cd52
loudonclear <loudon_cohen@brown.edu>
2017-10-14 16:09:21 -0400

    init commit

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+
+    public float fadeSpeed = 1.5f;
+
+    private SpriteRenderer sr;
+    private Color matCol;
+    private bool fade = false;
+    private float alfa;
+    private float fadeAmt;
+
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        alfa = sr.color.a;
+        fadeAmt = 1f / (fadeSpeed == 0 ? 1 : fadeSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fade)
+        {
+            Color matCol = sr.material.color;
+            alfa = matCol.a + Time.deltaTime * fadeAmt;
+            sr.material.SetColor("_Color", new Color(matCol.r, matCol.g, matCol.b, alfa));
+
+            print(alfa);
+            if (alfa <= 0)
+            {
+                gameObject.SetActive(false);
+                fade = false;
+                sr.material.SetColor("_Color", new Color(matCol.r, matCol.g, matCol.b, 0));
+            }
+            if (alfa >= 1)
+            {
+                gameObject.SetActive(true);
+                fade = false;
+                sr.material.SetColor("_Color", new Color(matCol.r, matCol.g, matCol.b, 1));
+            }
+        }
+    }
+
+    public void AltFade()
+    {
+        gameObject.SetActive(true);
+        fade = true;
+        fadeAmt = -fadeAmt;
+    }
+
+}
---
+++ Assets/Scripts/LampController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampController : MonoBehaviour {
+
+    public List<Fade> targets;
+    public AudioClip forward, backward;
+
+    private Animator animator;
+    private AudioSource audio;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnMouseDown()
+    {
+        for (int i = 0; i < targets.Count; i++)
+        {
+            Fade obj = targets[i];
+            obj.AltFade();
+        }
+
+        bool lit = animator.GetBool("lit");
+
+        if (lit)
+        {
+            audio.clip = backward;
+        } else
+        {
+            audio.clip = forward;
+        }
+
+        audio.Play();
+        animator.SetBool("lit", !lit);
+    }
+
+}
